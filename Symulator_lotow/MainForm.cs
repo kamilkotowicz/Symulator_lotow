c51c88ac61e13723cb966ee529bdf9ded983d40d
kamilkotowicz <kamil.kotowicz.k@gmail.com>
2022-05-15 19:31:51 +0200

    Dodano przykladowe wyswietlanie obiektow ruchomych

--- Symulator_lotow/MainForm.cs
+++ Symulator_lotow/MainForm.cs
@@ -20,39 +20,36 @@ namespace Symulator_lotow
             InitializeComponent();
         }
 
-        // This example creates a PictureBox control on the form and draws to it.
-        // This example assumes that the Form_Load event handler method is
-        // connected to the Load event of the form.
-
         private PictureBox pictureBox1 = new PictureBox();
-        // Cache font instead of recreating font objects each time we paint.
         private Font fnt = new Font("Arial", 10);
-        private void Form1_Load(object sender, System.EventArgs e)
+        private float skala_piksele_metry = 1F;
+        private void Form1_Load(object sender, System.EventArgs e)// funkcja musi byc polaczona ze zdarzeniem load
         {
             // Dock the PictureBox to the form and set its background to white.
             pictureBox1.Dock = DockStyle.Fill;
             pictureBox1.BackColor = Color.White;
             // Connect the Paint event of the PictureBox to the event handler method.
             pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
-
             // Add the PictureBox control to the Form.
             this.Controls.Add(pictureBox1);
         }
 
+        public int GetScreenHeight()
+        {
+            return pictureBox1.ClientSize.Height;
+        }
+
+        public int GetScreenWidth()
+        {
+            return pictureBox1.ClientSize.Width;
+        }
+
+
         private void pictureBox1_Paint(object sender, PaintEventArgs e)
         {
-            int maxX = pictureBox1.ClientSize.Width;
-            int maxY = pictureBox1.ClientSize.Height;
-            Debug.WriteLine(maxX);
-            Debug.WriteLine(maxY);
+            Debug.WriteLine(pictureBox1.ClientSize.Width);
+            Debug.WriteLine(pictureBox1.ClientSize.Height);
             Graphics g = e.Graphics;
-            /*
-            RysujProstokat(new Point(100, 100), new Point(30, 10), Color.Blue, g);
-            RysujKwadrat(new Point(200, 200), 20, Color.Red, g);
-            RysujKolo(new Point(300, 300), 20, Color.Yellow, g);
-            RysujTrojkat(new Point(400, 400), new Point(400, 420), new Point(420, 400), Color.Green, g);
-            
-            */
             Point[] points =
 {
                  new Point(10,  10),
@@ -71,7 +68,16 @@ namespace Symulator_lotow
             RysujNapis("Wiezowiec", Brushes.Red, new Point(400, 200), g);
             RysujKomin(new Point(500, 100), 14, g);
             RysujNapis("Komin", Brushes.Blue, new Point(500, 100), g);
-            
+            RysujSamolot(new Point(300, 100), g);
+            RysujNapis("Samolot", Brushes.Red, new Point(300, 100), g);
+            RysujDron(new Point(700, 100), g);
+            RysujNapis("Dron", Brushes.Red, new Point(700, 100), g);
+            RysujSmiglowiec(new Point(700, 200), g);
+            RysujNapis("Śmigłowiec", Brushes.Red, new Point(700, 200), g);
+            RysujBalon(new Point(700, 300), g);
+            RysujNapis("Balon", Brushes.Red, new Point(700, 300), g);
+            RysujSzybowiec(new Point(800, 300), g);
+            RysujNapis("Szybowiec", Brushes.Red, new Point(800, 300), g);
         }
 
         private void RysujProstokat(Point pos,Point size, Color kolor, Graphics g)
@@ -90,14 +96,11 @@ namespace Symulator_lotow
         {
             SolidBrush brush = new SolidBrush(kolor);
             g.FillEllipse(brush, pos.X, pos.Y, 2*r, 2*r);
-            //Pen blackPen = new Pen(Color.Black, 3);
-            //Rectangle rect = new Rectangle(pos.X, pos.Y, r, r);
-            //g.DrawRectangle(blackPen, rect);
         }
 
         private void RysujWielokat(Point[] wierzcholki, Color kolor, Graphics g)
         {
-            SolidBrush brush = new SolidBrush(Color.Blue);
+            SolidBrush brush = new SolidBrush(kolor);
             g.FillPolygon(brush, wierzcholki);
         }
 
@@ -136,12 +139,67 @@ namespace Symulator_lotow
         {
             RysujKwadrat(pos, bok, Color.Blue, g);
         }
+        public void RysujObiektLatajacy(Point pos_srodka, Color kolor,int rozmiar, Graphics g)
+        {
+            Point p1 = new Point(pos_srodka.X, pos_srodka.Y - rozmiar);
+            Point p2 = new Point(pos_srodka.X - rozmiar, pos_srodka.Y + rozmiar);
+            Point p3 = new Point(pos_srodka.X + rozmiar, pos_srodka.Y + rozmiar);
+            RysujTrojkat(p1, p2, p3, kolor, g);
+        }
+        public void RysujDron(Point pos_srodka, Graphics g)
+        {
+            RysujObiektLatajacy(pos_srodka, Color.Black,4, g);//rozmiar 4
+        }
+        public void RysujSamolot(Point pos_srodka, Graphics g)
+        {
+            RysujObiektLatajacy(pos_srodka, Color.Yellow,12, g); //rozmiar 12
+        }
 
-        //Dron czarny
-        //Samolot zolty
-        //Smiglowiec pomaranczowy
-        //Balon fioletowy
-        //Szybowiec czerwony
+        public void RysujSmiglowiec(Point pos_srodka, Graphics g)
+        {
+            RysujObiektLatajacy(pos_srodka, Color.Orange,10, g); //rozmiar 10
+        }
+
+        public void RysujBalon(Point pos_srodka, Graphics g)
+        {
+            RysujObiektLatajacy(pos_srodka, Color.Violet,6, g);//rozmiar 6
+        }
+
+        public void RysujSzybowiec(Point pos_srodka, Graphics g)
+        {
+            RysujObiektLatajacy(pos_srodka, Color.Red,8, g); //rozmiar 8
+        }
+        /*public void RysujObiekt(Object o, Graphics g)
+        {
+            int x, y, r, a, b;
+            if (o is ObiektyStale || o is StatekPowietrzny)
+            {
+                x = Convert.ToInt32(o.getX() * skala_piksele_metry);
+                y = Convert.ToInt32(o.getY() * skala_piksele_metry);
+            }
+            if (o is Drzewo)
+            {
+                r = Convert.ToInt32(o.getR() * skala_piksele_metry);
+                RysujDrzewo(new Point(x, y), r, g);
+            }
+            if(o is Komin)
+            {
+                r = Convert.ToInt32(o.getR() * skala_piksele_metry);
+                RysujKomin(new Point(x, y), r, g);
+            }
+            if(o is Blok)
+            {
+                pol_a = Convert.ToInt32(o.getA() * skala_piksele_metry/2.0);
+                pol_b = Convert.ToInt32(o.getB() * skala_piksele_metry/2.0);
+                RysujBlok(new Point(x - pol_a, y - pol_b), new Point(pol_a * 2, pol_b * 2), g);
+            }
+            if (o is Wiezowiec)
+            {
+                pol_a = Convert.ToInt32(o.getA() * skala_piksele_metry / 2.0);
+                RysujWiezowiec(new Point(x - pol_a, y - pol_a), pol_a * 2, g);
+            }
+        }*/
+
 
     }
 
