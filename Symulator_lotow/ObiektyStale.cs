022dd11ed71e58a55af71ab5a42dc4ba6c8892ad
Nataliaaa444 <altanka004@gmail.com>
2022-05-18 13:11:48 +0200

    dodano obiekty stale/Karol

---
+++ Symulator_lotow/ObiektyStale.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Symulator_lotow
+{
+
+    class Obiekty_stale
+    {
+        protected int id;
+        protected int wysokosc;
+        protected Punkt pozycja_srodka;
+
+    }
+
+    class Drzewo : Obiekty_stale
+    {
+        private readonly int promien;
+    }
+
+    class Blok : Obiekty_stale
+    {
+        private readonly int dlugosc;
+        private readonly int szerokosc;
+    }
+
+    class Wieżowiec : Obiekty_stale
+    {
+        private readonly int bok;
+    }
+
+    class Komin : Obiekty_stale
+    {
+        private readonly int promien;
+    }
+
+
+}
