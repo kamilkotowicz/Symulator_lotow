29c223b48fa58cef46caf617051e6a077d279785
kamilkotowicz <kamil.kotowicz.k@gmail.com>
2022-05-28 22:29:42 +0200

    Poprawione wyswietlanie napisow

--- Symulator_lotow/KontrolerRadaru.cs
+++ Symulator_lotow/KontrolerRadaru.cs
@@ -19,31 +19,26 @@ public class KontrolerRadaru
     {
         foreach (ObiektyStale os in symulator.obiekty_stale)
         {
+            string opis = os.nazwa + "\nWysokosc " + os.wysokosc;
+            Point pos = new Point((int)os.pozycja_srodka.x, (int)os.pozycja_srodka.y);
             if (os is Drzewo d)
             {
-                Point pos = new Point((int)d.pozycja_srodka.x, (int)d.pozycja_srodka.y);
                 ekran.RysujDrzewo(pos, (int)d.promien, g);
-                ekran.RysujNapis(d.nazwa, Brushes.Black, pos, g);
             }
             else if (os is Komin k)
             {
-                Point pos = new Point((int)k.pozycja_srodka.x, (int)k.pozycja_srodka.y);
                 ekran.RysujKomin(pos, (int)k.promien, g);
-                ekran.RysujNapis(k.nazwa, Brushes.Black, pos, g);
             }
             else if (os is Blok b)
             {
-                Point pos = new Point((int)b.pozycja_srodka.x, (int)b.pozycja_srodka.y);
                 Point rozm = new Point((int)b.dlugosc, (int)b.szerokosc);
                 ekran.RysujBlok(pos, rozm, g);
-                ekran.RysujNapis(b.nazwa, Brushes.Black, pos, g);
             }
             else if (os is Wiezowiec w)
             {
-                Point pos = new Point((int)w.pozycja_srodka.x, (int)w.pozycja_srodka.y);
                 ekran.RysujWiezowiec(pos, (int)w.bok, g);
-                ekran.RysujNapis(w.nazwa, Brushes.Black, pos, g);
             }
+            ekran.RysujNapis(opis, Brushes.Black, pos, g);
         }
     }
     void RysujRuchome(Graphics g)
@@ -52,7 +47,6 @@ public class KontrolerRadaru
         {
             Point pos = new Point((int)sp.aktualna_pozycja.x, (int)sp.aktualna_pozycja.y);
             string opis = sp.nazwa + "\nWysokosc " + sp.aktualna_pozycja.z + "\nPredkosc " + sp.trasa.predkosc;
-            ekran.RysujNapis(opis, Brushes.Black,pos, g);
             Point[] points =new Point[2];
             points[0] = pos;
             points[1] = new Point((int)sp.trasa.punkt_docelowy.x, (int)sp.trasa.punkt_docelowy.y);
@@ -61,7 +55,8 @@ public class KontrolerRadaru
             else if (sp is Samolot) ekran.RysujSamolot(pos, g);
             else if (sp is Balon) ekran.RysujBalon(pos, g);
             else if(sp is Smiglowiec) ekran.RysujSmiglowiec(pos, g);
-            else if(sp is Szybowiec) ekran.RysujSzybowiec(pos, g);             
+            else if(sp is Szybowiec) ekran.RysujSzybowiec(pos, g);
+            ekran.RysujNapis(opis, Brushes.Black, pos, g);
         }
     }
     public void RysujZawartoscPictureBoxa(object sender, PaintEventArgs e)
